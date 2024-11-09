b3ee4fce6f0808a95e4a2a64edc3e95ed6932565
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2024-11-09 23:59:38 +0900

    FlagGUIを表示するように

--- src/main/java/net/nekozouneko/playerguard/command/FlagsCommand.java
+++ src/main/java/net/nekozouneko/playerguard/command/FlagsCommand.java
@@ -1,9 +1,15 @@
 package net.nekozouneko.playerguard.command;
 
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import net.md_5.bungee.api.ChatColor;
+import net.nekozouneko.playerguard.PGUtil;
+import net.nekozouneko.playerguard.PlayerGuard;
+import net.nekozouneko.playerguard.gui.MenuGUI;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,6 +18,21 @@ public class FlagsCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.DARK_RED+"■ "+ChatColor.RED+"このコマンドはプレイヤーからのみ実行できます。");
+            return true;
+        }
+
+        Player p = (Player) sender;
+
+        ProtectedRegion pr = PGUtil.getCurrentPositionRegion(p);
+
+        if (pr == null || !pr.getOwners().contains(p.getUniqueId())) {
+            sender.sendMessage(ChatColor.DARK_RED+"■ "+ChatColor.RED+"ここにはあなたが管理できる保護領域がありません。");
+            return true;
+        }
+
+        new MenuGUI(PlayerGuard.getInstance(), p, pr).open();
         return true;
     }
 
